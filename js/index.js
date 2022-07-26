9f532731e35b44237c881266189fa14a4616e2d5
Abrar052 <bc160400820@vu.edu.pk>
2022-07-27 04:17:34 +0500

    Made the required changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 const navToggle = document.querySelector('#navToggle');
+const icon = document.querySelector('.fa-bars');
 const nav = document.querySelector('#nav-desktop');
+const brand = document.querySelector('#brand');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav-open');
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.replace('fa-bars', 'fa-times');
+    brand.style.display = 'none';
+  } else {
+    icon.classList.replace('fa-times', 'fa-bars');
+    brand.style.display = '';
+  }
 });
 
 document.querySelectorAll('.nav-link').forEach(() => nav.addEventListener('click', () => {
