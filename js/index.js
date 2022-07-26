450e8e1ce9f55de9cec6d4ea054bfd34386b07c9
Abrar052 <bc160400820@vu.edu.pk>
2022-07-26 08:52:18 +0500

    Solved linters Errors

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('#nav-desktop');
 
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+});
 
-const navToggle = document.querySelector("#navToggle");
-const nav = document.querySelector("#nav-desktop");
-
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle('nav-open');
-
-})
-
-document.querySelectorAll(".nav-link").forEach(n => nav.addEventListener("click", () => {
-    nav.classList.remove('nav-open');
-}))
+document.querySelectorAll('.nav-link').forEach(() => nav.addEventListener('click', () => {
+  nav.classList.remove('nav-open');
+}));
